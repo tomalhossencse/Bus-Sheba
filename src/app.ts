0eaca5d77a65715bd168e2b649e3903b7b4bf2ca
Tomal Hossen <tomalhossen78@gmail.com>
2026-09-02 22:55:43 +0600

    initialize project structure with essential configurations and setup

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, {
+	type Application,
+	type Request,
+	type Response,
+} from "express";
+import httpStatus from "http-status";
+import config from "./app/config";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
+
+const app: Application = express();
+
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+// app.use('/api/v1/auth', AuthRoutes)
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+	res.status(httpStatus.OK).json({
+		success: true,
+		message: "Welcome to PH Healthcare System Backend",
+	});
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+	node_env: process.env.NODE_ENV,
+	port: process.env.PORT,
+	database_url: process.env.DATABASE_URL,
+	bak_url: process.env.APP_URL,
+	frontend_url: process.env.FRONTEND_URL,
+	bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+	jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+	jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+	jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+	jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+	google_client_id: process.env.GOOGLE_CLIENT_ID!,
+	super_admin_name: process.env.SUPER_ADMIN_NAME!,
+	super_admin_email: process.env.SUPER_ADMIN_EMAIL!,
+	super_admin_password: process.env.SUPER_ADMIN_PASSWORD!,
+	tester_admin_name: process.env.TESTER_ADMIN_NAME!,
+	tester_admin_email: process.env.TESTER_ADMIN_EMAIL!,
+	tester_admin_password: process.env.TESTER_ADMIN_PASSWORD!,
+	tester_operator_name: process.env.TESTER_OPERATOR_NAME!,
+	tester_operator_email: process.env.TESTER_OPERATOR_EMAIL!,
+	tester_operator_password: process.env.TESTER_OPERATOR_PASSWORD!,
+	radis_username: process.env.RADIS_USERNAME!,
+	radis_password: process.env.RADIS_PASSWORD!,
+	radis_host: process.env.RADIS_HOST!,
+	radis_port: process.env.RADIS_PORT!,
+	smtp_user: process.env.SMTP_USER!,
+	smtp_password: process.env.SMTP_PASSWORD!,
+	email_sender: process.env.EMAIL_SENDER!,
+	cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+	cloudinary_cloud_api_key: process.env.CLOUDINARY_CLOUD_API_KEY!,
+	cloudinary_cloud_api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET!,
+	bkash_base_url: process.env.BKASH_BASE_URL!,
+	bkash_username: process.env.BKASH_USERNAME!,
+	bkash_password: process.env.BKASH_PASSWORD!,
+	bkash_app_key: process.env.BKASH_APP_KEY!,
+	bkash_app_secret: process.env.BKASH_APP_SECRET!,
+	bkash_callback_url: process.env.BKASH_CALLBACK_URL!,
+};
